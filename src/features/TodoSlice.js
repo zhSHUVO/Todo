23ed93327163014fb7be54fdff8b9be54dad67a6
zhSHUVO <zhshuvo99@gmail.com>
2022-08-07 12:06:29 +0600

    delete todo

--- src/features/TodoSlice.js
+++ src/features/TodoSlice.js
@@ -15,8 +15,12 @@ export const todoSlice = createSlice({
         addTodo: (state, action) => {
             state.todos.push(action.payload);
         },
+        deleteTodo: (state, action) => {
+            const id = action.payload;
+            state.todos = state.todos.filter((todo) => todo.id !== id);
+        },
     },
 });
 
-export const { showTodos, addTodo } = todoSlice.actions;
+export const { showTodos, addTodo, deleteTodo } = todoSlice.actions;
 export default todoSlice.reducer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo } from "../features/TodoSlice";
 
 const Home = () => {
     const todos = useSelector((state) => state.todosReducer.todos);
+    const dispatch = useDispatch();
+    const handleDelete = (id) => {
+        dispatch(deleteTodo(id));
+    };
 
     return (
         <div>
@@ -34,7 +39,12 @@ const Home = () => {
                                                 <path d="M257.7 752c2 0 4-.2 6-.5L431.9 722c2-.4 3.9-1.3 5.3-2.8l423.9-423.9a9.96 9.96 0 0 0 0-14.1L694.9 114.9c-1.9-1.9-4.4-2.9-7.1-2.9s-5.2 1-7.1 2.9L256.8 538.8c-1.5 1.5-2.4 3.3-2.8 5.3l-29.5 168.2a33.5 33.5 0 0 0 9.4 29.8c6.6 6.4 14.9 9.9 23.8 9.9zm67.4-174.4L687.8 215l73.3 73.3-362.7 362.6-88.9 15.7 15.6-89zM880 836H144c-17.7 0-32 14.3-32 32v36c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-36c0-17.7-14.3-32-32-32z"></path>
                                             </svg>
                                         </button>
-                                        <button className="btn btn-sm btn-error">
+                                        <button
+                                            onClick={() => {
+                                                handleDelete(id);
+                                            }}
+                                            className="btn btn-sm btn-error"
+                                        >
                                             <svg
                                                 stroke="currentColor"
                                                 fill="currentColor"
